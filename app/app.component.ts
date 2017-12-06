685ef50a48ce4e60543c56236e3fbc98f8685c3f
Emily Wells Jimenez and James Osborn <Guest@Epicodus-3A.local>
2017-12-05 16:23:32 -0800

    figure out how to show ingredients and instructions separately.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,18 +7,21 @@ import { Component } from '@angular/core';
     <h1>Recipe List</h1>
     <img src="/resources/img/recipe-box-1.jpg" alt="recipe box">
     <div class="well" *ngFor="let currentRecipe of recipes">
-      <ul>
+      <ul (click)="showRecipe(currentRecipe)">
         <li class="title">{{currentRecipe.title}}</li>
+        <div *ngIf="selectedRecipe">
           <ul>
             <li class="ingredients">Ingredients: {{currentRecipe.ingredients}}</li>
             <ul>
               <li class="instructions">instructions: {{currentRecipe.instructions}}</li>
             </ul>
           </ul>
+        </div>
       </ul>
     </div>
   </div>
   `
+  //per lina's advice, to show ingredients and instructions separately, put them in another div or well away from selectedRecipe.title.
 })
 
 export class AppComponent {
@@ -27,7 +30,16 @@ recipes: Recipe[] = [
   new Recipe("PB&J Sandwich", "Peanut butter, jelly, bread", "1. slather jelly and peanut button each slice of bread, 2. Smash the two pieces together and enjoy."),
   new Recipe("Cereal", "Cereal, milk", "1. Pour cereal into bowl, 2. Pour milk over cereal, 3. Eat cereal.")
 ];
+
+selectedRecipe = null;
+
+showRecipe(viewRecipe) {
+  this.selectedRecipe = viewRecipe;
 }
+
+}
+
+
 
 export class Recipe {
   constructor(public title: string, public ingredients: string, public instructions: string) { }
